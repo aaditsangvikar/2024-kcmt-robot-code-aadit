3e762efe1a5e7da1e4af21f66ef70522de9f7b7e
aaditsangvikar <aadit.sangvikar@gmail.com>
2024-09-13 01:30:20 -0600

    Faster intake execution

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -2,11 +2,16 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class IntakeCommand extends Command {
     private final IntakeSubsystem intakeSubsystem;
 
     private boolean continuous;
+
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     /**
      * Command to intake a note
@@ -19,17 +24,27 @@ public class IntakeCommand extends Command {
         addRequirements(subsystem);
     }
 
-    @Override
-    public void execute() {
-        if (!this.intakeSubsystem.getFrontLinebreak() || continuous) {
-            intakeSubsystem.setSpeed(0.6);
+    public void initialize() {
+        if (!continuous) {
+            // Asynchronously run the intake motor, checking the linebreak every 5 milliseconds
+            executor.scheduleAtFixedRate(() -> {
+                if (!this.intakeSubsystem.getFrontLinebreak()) {
+                    intakeSubsystem.setSpeed(0.6);
+                } else {
+                    intakeSubsystem.setSpeed(0);
+                }
+            }, 0, 5, TimeUnit.MILLISECONDS);
         } else {
-            intakeSubsystem.setSpeed(0);
+            // Asynchronously run the intake motor every 20 milliseconds
+            executor.scheduleAtFixedRate(() -> {
+                intakeSubsystem.setSpeed(0.6);
+            }, 0, 20, TimeUnit.MILLISECONDS);
         }
     }
 
     @Override
     public void end(boolean interrupted) {
+        executor.shutdown();
         intakeSubsystem.setSpeed(0);
     }
 }
